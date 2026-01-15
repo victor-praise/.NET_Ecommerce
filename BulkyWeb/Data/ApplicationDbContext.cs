2f1b773972132fb7acf48286ec6b5204cc41735e
victor-praise <61267951+victor-praise@users.noreply.github.com>
2026-01-15 12:16:49 -0500

    Added create functionality

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -12,5 +12,15 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { ID = 1, Name = "Books", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "Movies", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "Music", DisplayOrder = 3 }
+                );
+        }
     }
 }
